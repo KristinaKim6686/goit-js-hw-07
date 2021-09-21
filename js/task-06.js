34683199ff4ce1e0ee9b6ec98e0143c55f0ad7ef
kim <82406962+KristinaKim6686@users.noreply.github.com>
2021-09-21 23:14:28 +0300

    6th fixed

--- js/task-06.js
+++ js/task-06.js
@@ -1,16 +1,14 @@
 const inputRef = document.querySelector('#validation-input');
-const inputLength = inputRef.dataset.length;
+const inputLength = Number(inputRef.dataset.length);
 
 inputRef.addEventListener('change', input => {
     const text = input.target.value;
 
-    if (text.length == inputLength){
+    if (text.length === inputLength) {
         inputRef.classList.add('valid');
-        inputRef.classList.remove('invalid')
     }
     else {
         inputRef.classList.add('invalid')
-        inputRef.classList.remove('valid')
     }
 }
 
